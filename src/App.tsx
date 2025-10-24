ea14fe32a0820e6ca7c565f0b25c03345601513d
Texas096 <167821055+Texas096@users.noreply.github.com>
2025-10-24 17:23:15 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import UserData from "./UserData";
+
+function App() {
+  
+  // define an array of objects
+  
+  const data = [
+    {name: "Maria Rodriguez",
+      title: "Marketing Manager",
+      bio: "Manages digital marketing and social media strategy. Highly skilled in SEO and content creation.",
+    }
+  ]
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen w-screen ">
+      <div className="w-[400px}">
+        <div className="bg-cyan-600 text-white p-5 outline outline-cyan-700 rounded-t-lg">
+          <h1 className="text-lg text-cyan-100">{data[0].bio}</h1>
+          <h1>hello</h1>
+        </div>
+      </div>
+      <UserData/>
+    </div>
+  )
+
+}
+
+export default App
---
+++ src/UserData.tsx
@@ -0,0 +1,13 @@
+export UserData
+
+const UserData = ({data}) => {
+    return (
+        <div className="mb-5">
+            <p>Name: {data.name}</p>
+            <p>Title: {data.title}</p>
+            <p>bio: {data.bio}</p>
+            <p>square of number: {data.sqNum}</p>
+            
+        </div>
+    )
+}
